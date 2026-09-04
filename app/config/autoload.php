347a4bcbd47018c0b1f281153441ffa5a06b6943
Dan <daniellevitto271@gmail.com>
2026-09-04 14:24:54 +0800

    auto load/ routes

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -72,8 +72,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 |	$autoload['libraries'] = array('database', 'email', 'session');
 */
-$autoload['libraries'] = array();
-
+$autoload['libraries'] = array('database', 'session');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Helper Files
@@ -82,7 +81,7 @@ $autoload['libraries'] = array();
 |
 |	$autoload['helpers'] = array('url', 'file');
 */
-$autoload['helpers'] = array();
+$autoload['helpers'] = array('url');
 
 /*
 | -------------------------------------------------------------------
@@ -106,6 +105,5 @@ $autoload['models'] = array();
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['configs'] = array();
-$autoload['helpers'] = array('url');
+$autoload['configs'] = array('middleware');
 ?>
--- app/config/routes.php
+++ app/config/routes.php
@@ -48,3 +48,5 @@ $router->get('/', 'Welcome::index');
 
 $router->get('student', 'StudentController::index');
 $router->get('student/profile', 'StudentController::profile', ['middleware' => 'StudentMiddleware']);
+
+$router->get('/users', 'UsersController::index');
